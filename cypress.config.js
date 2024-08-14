164c53a941086f0afd5f1a106f6e4047efd8ce98
dominikanykiel <dominika.nykiel.tests@gmail.com>
2024-08-14 18:45:41 +0200

    configured formatters

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  trashAssetsBeforeRuns: true,
+  chromeWebSecurity: false,
+  experimentalWebKitSupport: true,
+  experimentalMemoryManagement: true,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
-    setupNodeEvents(on, config) {
-    },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
+    setupNodeEvents(on, config) {},
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
   },
 });
